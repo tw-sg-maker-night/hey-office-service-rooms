1a1ae9fc58c77b3e7a3f15de254124207fa2104f
Albert Salim <asalim@thoughtworks.com>
2017-01-07 16:27:13 +0800

    Add aws lambda client to call meeting room booking function

--- handler.js
+++ handler.js
@@ -1,20 +1,34 @@
 'use strict';
 
 const moment = require('moment-timezone');
+const aws = require('aws-sdk');
 
 module.exports.rooms = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  const params = event.result.parameters;
+  const reservation = this.createReservation(params)
 
-  callback(null, response);
+  const lambda = new aws.Lambda({
+    region: 'ap-southeast-1'
+  });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  lambda.invoke({
+    FunctionName: 'hey-office-meetings-dev-create',
+    Payload: JSON.stringify({body: JSON.stringify(reservation)})
+  }, (err, data) => {
+    let message = 'Unable to reserve the room, please try again.';
+    if (!err) {
+      const responsePayload = JSON.parse(data.Payload);
+      const result = JSON.parse(responsePayload.body);
+      message = result.message;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: { message: message }
+    };
+
+    callback(null, response);
+  });
 };
 
 module.exports.createReservation = (params) => {
